e2f37eeda16f01c27cbd5d7770af24eec37cf8fa
Bruno Santos <brcesar19@gmail.com>
2020-08-21 22:27:39 -0300

    Funcionalidades de add/remover e testes passando

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(()=>{
+    // Busca as listas da API
+    api.get('repositories').then(response=>{
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+   const newRepo = await api.post('repositories', {
+     title: 'Novo Projeto',
+     url: 'http://novoprojeto.dev',
+     techs: ['Node.Js', 'ReatcJS']
+   });
+   setRepositories([...repositories, newRepo.data]); // Atualiza lista de repositórios
+   return newRepo;
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`); // Deleta repositório da API
+
+    setRepositories(repositories.filter(repository=> repository.id !== id)) // Exibie somente os repositórios que não foi o deletado
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository=> (
+          <li key={repository.id}>
+          {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        ))}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
